682dc58202dac765e9337c477864480441c63500
Vyacheslav <slava.shilin07@mail.ru>
2022-12-10 15:34:48 +0300

    Initial commit

---
+++ CustomFlagComponent.cs
@@ -0,0 +1,135 @@
+﻿using BepInEx.Logging;
+using System;
+using System.Collections;
+using UnityEngine;
+
+#pragma warning disable CS8632
+namespace CustomShipFlags
+{
+    public class CustomFlagComponent : MonoBehaviour, Interactable, TextReceiver, Hoverable
+    {
+        private ZNetView? m_nview;
+        private Renderer renderer;
+        private BoxCollider collider;
+
+        private void Awake()
+        {
+            m_nview = transform.parent.parent.parent.parent.parent.gameObject.GetComponent<ZNetView>();
+            m_nview.Register("SetFlagUrl", new Action<long, string>(RPC_SetCustomFlag));
+            renderer = GetComponent<Renderer>();
+            collider = gameObject.AddComponent<BoxCollider>();
+            collider.isTrigger = true;
+            collider.center = new(0, -6.4f, 0);
+            collider.size = new(-0.23f, 21, 4);
+
+            Plugin._self.Config.ConfigReloaded += (_, _) => { LoadSavedTextureFromURL(); };
+
+
+            LoadSavedTextureFromURL();
+        }
+
+        public bool Interact(Humanoid human, bool hold, bool alt)
+        {
+            if (Plugin.useOnlyServerFlag)
+            {
+                human?.Message(MessageHud.MessageType.Center, "$piece_noaccess", 0, null);
+                return false;
+            }
+
+            if (hold)
+            {
+                return false;
+            }
+            if (!PrivateArea.CheckAccess(transform.position, 0f, true, false))
+            {
+                human?.Message(MessageHud.MessageType.Center, "$piece_noaccess", 0, null);
+                return true;
+            }
+            TextInput.instance.RequestText(this, "Enter Texture Url", 9999);
+            return true;
+        }
+        public string GetText()
+        {
+            return m_nview?.GetZDO()?.GetString("TextureUrl", "");
+        }
+        public void SetText(string text)
+        {
+            if (!m_nview.IsValid())
+            {
+                return;
+            }
+            m_nview?.InvokeRPC("SetFlagUrl", new object[]
+            {
+                text
+            });
+        }
+        void RPC_SetCustomFlag(long sender, string url)
+        {
+            if (!m_nview.IsValid() || !m_nview.IsOwner())
+            {
+                return;
+            }
+            if (GetText() == url)
+            {
+                return;
+            }
+            m_nview.GetZDO().Set("TextureUrl", url);
+            SetTextureFromURL(url);
+        }
+
+        public bool UseItem(Humanoid user, ItemDrop.ItemData item) { return false; }
+        public string GetHoverText()
+        {
+            return "Set Flag Url";
+        }
+        public string GetHoverName()
+        {
+            return "";
+        }
+
+
+        public void SetTextureFromURL(string url)
+        {
+            if (string.IsNullOrEmpty(url) || string.IsNullOrWhiteSpace(url)) return;
+            StartCoroutine(SetTextureFromURLIEnumerator(url, renderer));
+        }
+        public void LoadSavedTextureFromURL()
+        {
+            string url;
+            if (Plugin.useOnlyServerFlag)
+            {
+                url = Plugin.serverFlagUrl;
+            }
+            else
+            {
+                url = GetText();
+                if (string.IsNullOrEmpty(url) || string.IsNullOrWhiteSpace(url))
+                {
+                    url = Plugin.serverFlagUrl;
+                }
+                if (string.IsNullOrEmpty(url) || string.IsNullOrWhiteSpace(url))
+                {
+                    Plugin._self.Debug(LogLevel.Warning, "The server flag is missing, the vanilla version will be used.");
+                    return;
+                }
+            }
+
+            if (url != GetText())
+            {
+                m_nview?.GetZDO()?.Set("TextureUrl", url);
+            }
+
+            StartCoroutine(SetTextureFromURLIEnumerator(url, renderer));
+        }
+        private IEnumerator SetTextureFromURLIEnumerator(string url, Renderer renderer)
+        {
+            Plugin._self.Debug(LogLevel.Info, "Loading...");
+            WWW wwwLoader = new(url);
+            yield return wwwLoader;
+
+            renderer.material.color = Color.white;
+            renderer.material.mainTexture = wwwLoader.texture;
+            Plugin._self.Debug(LogLevel.Info, "Loaded");
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,127 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using ServerSync;
+using System;
+using System.IO;
+
+#pragma warning disable CS8632
+namespace CustomShipFlags
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        #region variables
+        public const string ModName = "CustomShipFlags", ModVersion = "1.0.0", ModGUID = "com.Frogger." + ModName;
+        private static Harmony harmony = new(ModGUID);
+        public static Plugin _self;
+        private const string path = "ship/visual/Mast/Sail/sail_full";
+        #endregion
+
+        #region ConfigSettings
+        static string ConfigFileName = "com.Frogger.CustomShipFlags.cfg";
+        DateTime LastConfigChange;
+        public static ConfigSync configSync = new(ModName) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+        ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
+        {
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        public enum Toggle
+        {
+            On = 1,
+            Off = 0
+        }
+        
+        static ConfigEntry<string> serverFlagUrlConfig;
+        static ConfigEntry<Toggle> useOnlyServerFlagConfig;
+        public static string serverFlagUrl = "";
+        public static bool useOnlyServerFlag = false;
+        #endregion
+
+        private void Awake()
+        {
+            _self = this;
+            Config.SaveOnConfigSet = false;
+            
+            harmony.PatchAll();
+
+            serverFlagUrlConfig = config("General", "FlagURL", "", new ConfigDescription("The server flag url", null, new ConfigurationManagerAttributes { HideSettingName = true, HideDefaultButton = true }));
+            useOnlyServerFlagConfig = config("General", "useOnlyServerFlag", Toggle.Off, "");
+
+            SetupWatcher();
+
+            Config.ConfigReloaded += (_, _) => { UpdateConfiguration(); };
+
+            Config.SaveOnConfigSet = true;
+
+            Config.Save();
+        }
+        private void SetupWatcher()
+        {
+            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher(Paths.ConfigPath, ConfigFileName);
+            fileSystemWatcher.Changed += ConfigChanged;
+            fileSystemWatcher.IncludeSubdirectories = true;
+            fileSystemWatcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            fileSystemWatcher.EnableRaisingEvents = true;
+        }
+        void ConfigChanged(object sender, FileSystemEventArgs e)
+        {
+            if ((DateTime.Now - this.LastConfigChange).TotalSeconds <= 5.0)
+            {
+                return;
+            }
+            LastConfigChange = DateTime.Now;
+            try
+            {
+                Config.Reload();
+                Debug(LogLevel.Info, "Reloading Config...");
+            }
+            catch
+            {
+                Debug(LogLevel.Error, "Can't reload Config");
+            }
+        }
+        private void UpdateConfiguration()
+        {
+            serverFlagUrl = serverFlagUrlConfig.Value;
+            useOnlyServerFlag = useOnlyServerFlagConfig.Value == Toggle.On ? true : false;
+            Debug(LogLevel.Info, "Configuration Received");
+        }
+
+        public void Debug(LogLevel level, string msg)
+        {
+            Logger.Log(level, msg);
+        }
+        public void Debug(string msg)
+        {
+            Logger.LogInfo(msg);
+        }
+
+        [HarmonyPatch] public static class Path
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(Ship), nameof(Ship.Awake))]
+            private static void ShipAwakePatch(Ship __instance)
+            {
+                __instance.transform.Find(path)?.gameObject.AddComponent<CustomFlagComponent>();
+            }
+        }
+
+        public class ConfigurationManagerAttributes
+        {
+            public int? Order;
+            public bool? HideSettingName;
+            public bool? HideDefaultButton;
+            public string? DispName;
+            public Action<ConfigEntryBase>? CustomDrawer;
+        }
+    }
+}
